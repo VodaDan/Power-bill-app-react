a29ec1a1a367a846ee8953592aea1f5327aaac5d
Dan <voda.daniel98@yahoo.com>
2025-05-15 23:51:50 +0300

    fix: fixed styling for home page

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,7 +1,10 @@
 const Home = () => {
     console.log(localStorage.getItem("email"));
     return (
-        <h1 className="home-page">Welcome to our page!</h1>
+        <div className="home-page">
+            <h1>Welcome to our page!</h1>
+            <h2>- Power bill management -</h2>
+        </div>
     );
 };
 
